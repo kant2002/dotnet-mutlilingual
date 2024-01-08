cf9902db6fd5e092efe2ccdaef96e1a16f95b4c8
Andrii Kurdiumov <kant2002@gmail.com>
2024-01-08 19:53:11 +0600

    Додав переклад на українську

---
+++ Українська/Система.Безпека.Стверждення/ТипиЗначеньСтверждень.cs
@@ -0,0 +1,145 @@
+﻿using System.Security.Claims;
+
+namespace Система.Безпека.Стверждення;
+
+/// <summary>
+/// Визначає типи значень стверждень відповідно до URI типів, визначених W3C і OASIS.
+/// Цей клас не можна успадкувати.
+/// </summary>
+public static class ТипиЗначеньСтверждень
+{
+	/// <summary>
+	/// URI, який представляє тип даних XML base64Binary.
+	/// </summary>
+	public static string БінарніБаза64 => ClaimValueTypes.Base64Binary;
+
+	/// <summary>
+	/// URI, який представляє тип даних SOAP UPN.
+	/// </summary>
+	public static string ІмяUpn => ClaimValueTypes.UpnName;
+
+	/// <summary>
+	/// URI, який представляє тип даних XML uinteger64.
+	/// </summary>
+	public static string БЦіле64 => ClaimValueTypes.UInteger64;
+
+	/// <summary>
+	/// URI, який представляє тип даних XML uinteger32.
+	/// </summary>
+	public static string БЦіле32 => ClaimValueTypes.UInteger32;
+
+	/// <summary>
+	/// URI, який представляє тип даних XML time.
+	/// </summary>
+	public static string Час => ClaimValueTypes.Time;
+
+	/// <summary>
+	/// URI, який представляє тип даних XML string.
+	/// </summary>
+	public static string Рядок => ClaimValueTypes.String;
+
+	/// <summary>
+	/// URI, який представляє тип даних XML sid.
+	/// </summary>
+	public static string Sid => ClaimValueTypes.Sid;
+
+	/// <summary>
+	/// URI, який представляє тип даних RSAKeyValue XML Signature.
+	/// </summary>
+	public static string RsaKeyValue => ClaimValueTypes.RsaKeyValue;
+
+	/// <summary>
+	/// URI, який представляє тип даних rsa SOAP.
+	/// </summary>
+	public static string Rsa => ClaimValueTypes.Rsa;
+
+	/// <summary>
+	/// URI, який представляє тип даних rfc822Name XACML 1.0.
+	/// </summary>
+	public static string Rfc822Name => ClaimValueTypes.Rfc822Name;
+
+	/// <summary>
+	/// URI, який представляє тип даних KeyInfo XML Signature.
+	/// </summary>
+	public static string KeyInfo => ClaimValueTypes.KeyInfo;
+
+	/// <summary>
+	/// URI, який представляє тип даних XML integer64.
+	/// </summary>
+	public static string Ціле64 => ClaimValueTypes.Integer64;
+
+	/// <summary>
+	/// URI, який представляє тип даних x500Name XACML 1.0.
+	/// </summary>
+	public static string X500Name => ClaimValueTypes.X500Name;
+
+	/// <summary>
+	/// URI, який представляє тип даних XML integer32.
+	/// </summary>
+	public static string Ціле32 => ClaimValueTypes.Integer32;
+
+	/// <summary>
+	/// URI, який представляє тип даних XML hexBinary.
+	/// </summary>
+	public static string БінарніГекс => ClaimValueTypes.HexBinary;
+
+	/// <summary>
+	/// URI, який представляє тип даних XML fqbn.
+	/// </summary>
+	public static string Fqbn => ClaimValueTypes.Fqbn;
+
+	/// <summary>
+	/// URI, який представляє тип даних SOAP emailaddress.
+	/// </summary>
+	public static string Пошта => ClaimValueTypes.Email;
+
+	/// <summary>
+	/// URI, який представляє тип даних DSAKeyValue XML Signature.
+	/// </summary>
+	public static string DsaKeyValue => ClaimValueTypes.DsaKeyValue;
+
+	/// <summary>
+	/// URI, який представляє тип даних XML double.
+	/// </summary>
+	public static string Подвійне => ClaimValueTypes.Double;
+
+	/// <summary>
+	/// URI, який представляє тип даних SOAP dns.
+	/// </summary>
+	public static string ІмяDns => ClaimValueTypes.DnsName;
+
+	/// <summary>
+	/// URI, який представляє тип даних XQuery daytimeDuration.
+	/// </summary>
+	public static string DaytimeDuration => ClaimValueTypes.DaytimeDuration;
+
+	/// <summary>
+	/// URI, який представляє тип даних XML dateTime.
+	/// </summary>
+	public static string ДатаІЧас => ClaimValueTypes.DateTime;
+
+	/// <summary>
+	/// URI, який представляє тип даних XML date.
+	/// </summary>
+	public static string Дата => ClaimValueTypes.Date;
+
+	/// <summary>
+	/// URI, який представляє тип даних XML boolean.
+	/// </summary>
+	public static string Логічне => ClaimValueTypes.Boolean;
+
+	/// <summary>
+	/// URI, який представляє тип даних XML base64Octet.
+	/// </summary>
+	public static string ОктетБаза64 => ClaimValueTypes.Base64Octet;
+
+	/// <summary>
+	/// URI, який представляє тип даних XML integer.
+	/// </summary>
+	public static string Ціле => ClaimValueTypes.Integer;
+
+	/// <summary>
+	/// URI, який представляє тип даних XQuery yearMonthDuration.
+	/// </summary>
+	public static string YearMonthDuration => ClaimValueTypes.YearMonthDuration;
+}
---
+++ Українська/Система.Безпека.Стверждення/ТипиСтверждень.cs
@@ -0,0 +1,111 @@
+﻿using System.Security.Claims;
+
+namespace Система.Безпека.Стверждення;
+
+/// <summary>
+/// Визначає константи для відомих типів стверждень, які можна призначити суб’єкту.
+/// Цей клас не можна успадкувати.
+/// </summary>
+public static class ТипиСтверждень
+{
+	/// <summary>
+	/// http://schemas.xmlsoap.org/ws/2005/05/identity/claims/actor.
+	/// </summary>
+	public static string Актор => ClaimTypes.Actor;
+
+	/// <summary>
+	/// URI для ствердження, яке визначає поштовий індекс організації, http://schemas.xmlsoap.org/ws/2005/05/identity/claims/postalcode.
+	/// </summary>
+	public static string Індекс => ClaimTypes.PostalCode;
+
+	/// <summary>
+	/// URI для ствердження, яке визначає головний SID групи сутності, http://schemas.xmlsoap.org/ws/2005/05/identity/claims/primarygroupsid.
+	/// </summary>
+	public static string ГоловнийSidГрупи => ClaimTypes.PrimaryGroupSid;
+
+	/// <summary>
+	/// URI для ствердження, яке визначає головний SID сутності, http://schemas.xmlsoap.org/ws/2005/05/identity/claims/primarysid.
+	/// </summary>
+	public static string ГоловнийSid => ClaimTypes.PrimarySid;
+
+	/// <summary>
+	/// URI для ствердження, яке визначає роль сутності, http://schemas.xmlsoap.org/ws/2005/05/identity/claims/role.
+	/// </summary>
+	public static string Роль => ClaimTypes.Role;
+
+	/// <summary>
+	/// URI для ствердження, яке визначає RSA ключ, http://schemas.xmlsoap.org/ws/2005/05/identity/claims/rsa.
+	/// </summary>
+	public static string Rsa => ClaimTypes.Rsa;
+
+	/// <summary>
+	/// URI для ствердження, яке визначає серійний номер, http://schemas.xmlsoap.org/ws/2005/05/identity/claims/serialnumber.
+	/// </summary>
+	public static string СерійнийНомер => ClaimTypes.SerialNumber;
+
+	/// <summary>
+	/// URI для ствердження, яке визначає ідентифікатор безпеки (SID), http://schemas.xmlsoap.org/ws/2005/05/identity/claims/sid.
+	/// </summary>
+	public static string Sid => ClaimTypes.Sid;
+
+	/// <summary>
+	/// URI для ствердження, яке визначає стверждення імені принципала служби (SPN), http://schemas.xmlsoap.org/ws/2005/05/identity/claims/spn.
+	/// </summary>
+	public static string Spn => ClaimTypes.Spn;
+
+	/// <summary>
+	/// URI для ствердження, яке визначає штат або провінцію, у якій проживає суб’єкт, http://schemas.xmlsoap.org/ws/2005/05/identity/claims/stateorprovince.
+	/// </summary>
+	public static string ШтатАбоПровінція => ClaimTypes.StateOrProvince;
+
+	/// <summary>
+	/// URI для ствердження, яке визначає вуличну адресу сутності, http://schemas.xmlsoap.org/ws/2005/05/identity/claims/streetaddress.
+	/// </summary>
+	public static string Адреса => ClaimTypes.StreetAddress;
+
+	/// <summary>
+	/// URI для ствердження, яке визначає фамілію сутності, http://schemas.xmlsoap.org/ws/2005/05/identity/claims/surname.
+	/// </summary>
+	public static string Фамілія => ClaimTypes.Surname;
+
+	/// <summary>
+	/// URI для ствердження, яке визначає системну сутність, http://schemas.xmlsoap.org/ws/2005/05/identity/claims/system.
+	/// </summary>
+	public static string Система => ClaimTypes.System;
+
+	/// <summary>
+	/// URI для ствердження, яке визначає цифровий відбиток, http://schemas.xmlsoap.org/ws/2005/05/identity/claims/thumbprint.
+	/// Відбиток – це глобально унікальний хеш SHA-1 сертифіката X.509.
+	/// </summary>
+	public static string Відбиток => ClaimTypes.Thumbprint;
+
+	/// <summary>
+	/// URI для ствердження, яке визначає основне ім’я користувача (UPN), http://schemas.xmlsoap.org/ws/2005/05/identity/claims/role.
+	/// </summary>
+	public static string Upn => ClaimTypes.Upn;
+
+	/// <summary>
+	/// URI для ствердження, яке визначає роль URI, http://schemas.xmlsoap.org/ws/2005/05/identity/claims/uri.
+	/// </summary>
+	public static string Uri => ClaimTypes.Uri;
+
+	/// <summary>
+	/// http://schemas.xmlsoap.org/ws/2005/05/identity/claims/userdata.
+	/// </summary>
+	public static string ДаніКористувача => ClaimTypes.UserData;
+
+	/// <summary>
+	/// http://schemas.xmlsoap.org/ws/2005/05/identity/claims/version.
+	/// </summary>
+	public static string Версія => ClaimTypes.Version;
+
+	/// <summary>
+	/// URI для ствердження, яке визначає веб-сторінку сутності, http://schemas.xmlsoap.org/ws/2005/05/identity/claims/webpage.
+	/// </summary>
+	public static string ВебСторінка => ClaimTypes.Webpage;
+
+	/// <summary>
+	/// URI для ствердження, яке визначає назву сутності, http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name.
+	/// </summary>
+	public static string Імя => ClaimTypes.Name;
+}

72ce50033f2de250686d2a86c2874696faa5f56c
Andrii Kurdiumov <kant2002@gmail.com>
2024-01-09 14:59:02 +0600

    Додав переклад українською

---
+++ Українська/Система.Безпека.Ствердження/ОсобистістьСтверждень.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Security.Claims;
+using System.Security.Principal;
+
+namespace Система.Безпека.Ствердження;
+
+public class ОсобистістьСтверждень : ClaimsIdentity
+{
+	public const string ВипускаючаСторонаЗаЗамовчанням = DefaultIssuer;
+	public const string ТипСтвержденняІменіЗаЗамовчанням = DefaultNameClaimType;
+	public const string ТипСтвержденняРольЗаЗамовчанням = DefaultRoleClaimType;
+
+	public ОсобистістьСтверждень()
+	{
+	}
+
+	public ОсобистістьСтверждень(IEnumerable<Claim> ствердження) : base(ствердження)
+	{
+	}
+
+	public ОсобистістьСтверждень(BinaryReader читач) : base(читач)
+	{
+	}
+
+	public ОсобистістьСтверждень(IIdentity особистість) : base(особистість)
+	{
+	}
+
+	public ОсобистістьСтверждень(string типАутентіфікації) : base(типАутентіфікації)
+	{
+	}
+
+	public ОсобистістьСтверждень(IEnumerable<Claim> стверждення, string типАутентіфікації) : base(стверждення, типАутентіфікації)
+	{
+	}
+
+	public ОсобистістьСтверждень(IIdentity особистість, IEnumerable<Claim> стверждення) : base(особистість, стверждення)
+	{
+	}
+
+	public ОсобистістьСтверждень(string типАутентіфікації, string типІмені, string типРолі) : base(типАутентіфікації, типІмені, типРолі)
+	{
+	}
+
+	public ОсобистістьСтверждень(IEnumerable<Claim> стверждення, string типАутентіфікації, string типІмені, string типРолі) : base(стверждення, типАутентіфікації, типІмені, типРолі)
+	{
+	}
+
+	public ОсобистістьСтверждень(IIdentity особистість, IEnumerable<Claim> стверждення, string типАутентіфікації, string типІмені, string типРолі) : base(особистість, стверждення, типАутентіфікації, типІмені, типРолі)
+	{
+	}
+
+	protected ОсобистістьСтверждень(SerializationInfo інфо) : base(інфо)
+	{
+	}
+
+	protected ОсобистістьСтверждень(ClaimsIdentity інша) : base(інша)
+	{
+	}
+
+	protected ОсобистістьСтверждень(SerializationInfo інфо, StreamingContext контекст) : base(інфо, контекст)
+	{
+	}
+
+	public virtual string ТипАутентіфікації => this.AuthenticationType;
+
+	public virtual string Імя => this.Name;
+	public string Помітка { get => this.Label; set => this.Label = value; }
+	public virtual bool Аутентіфіковано => this.IsAuthenticated;
+	public virtual IEnumerable<Claim> Стверждення => this.Claims;
+	public ClaimsIdentity Актор { get => this.Actor; set => this.Actor = value; }
+	public string ТипСтвержденняРоль => this.NameClaimType;
+	public string ТипСтвержденняІмя => this.NameClaimType;
+	public virtual void ДодатиСтверждення(Claim claim) => this.AddClaim(claim);
+	public virtual void ДодатиСтверждення(IEnumerable<Claim> claims) => this.AddClaims(claims);
+	public virtual ClaimsIdentity Склонувати() => this.Clone();
+
+	public virtual IEnumerable<Claim> ЗнайтиУсі(Predicate<Claim> співставлення) => this.FindAll(співставлення);
+
+	public virtual IEnumerable<Claim> ЗнайтиУсі(string тип) => this.FindAll(тип);
+
+	public virtual Claim ЗнайтиПерше(string тип) => this.FindFirst(тип);
+
+	public virtual Claim ЗнайтиПерше(Predicate<Claim> співставлення) => this.FindFirst(співставлення);
+
+	public virtual bool ІснуєСтверждення(Predicate<Claim> співставлення) => this.HasClaim(співставлення);
+
+	public virtual bool ІснуєСтверждення(string тип, string значення) => this.HasClaim(тип, значення);
+
+	public virtual void ВидалитиСтверждення(Claim claim) => this.RemoveClaim(claim);
+
+	public virtual bool СпробуватиВидалитиСтверждення(Claim claim) => this.TryRemoveClaim(claim);
+}
---
+++ Українська/Система.Безпека.Ствердження/Субєкт.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Claims;
+using System.Security.Principal;
+
+namespace Система.Безпека.Ствердження;
+
+public class СубєктСтверджень : ClaimsPrincipal
+{
+	public СубєктСтверджень()
+		: base()
+	{
+	}
+
+	public СубєктСтверджень(IEnumerable<ClaimsIdentity> identities)
+		: base(identities)
+	{
+	}
+
+	public СубєктСтверджень(BinaryReader читач)
+		: base(читач)
+	{
+	}
+
+	public СубєктСтверджень(IIdentity особистість)
+		: base(особистість)
+	{
+	}
+
+	public СубєктСтверджень(IPrincipal субєкт)
+		: base(субєкт)
+	{
+	}
+
+	public static ClaimsPrincipal Поточний => Current;
+
+	public static Func<ClaimsPrincipal> СелекторСубєкта
+	{
+		get => ClaimsPrincipalSelector;
+		set => ClaimsPrincipalSelector = value;
+	}
+
+	public static Func<IEnumerable<ClaimsIdentity>, ClaimsIdentity> СелекторГоловноїОсобистості
+	{
+		get => PrimaryIdentitySelector;
+		set => PrimaryIdentitySelector = value;
+	}
+
+	public virtual IIdentity Особистість => this.Identity;
+
+	public virtual IEnumerable<ClaimsIdentity> Особистості => Identities;
+	public virtual IEnumerable<Claim> Ствердження => Claims;
+
+	public virtual void ДодатиОсобистості(IEnumerable<ClaimsIdentity> особистості)
+	{
+		this.AddIdentities(особистості);
+	}
+
+	public virtual void ДодатиОсобистость(ClaimsIdentity identity)
+	{
+		this.AddIdentity(identity);
+	}
+
+	public virtual ClaimsPrincipal Склонувати() => this.Clone();
+
+	public virtual IEnumerable<Claim> ЗнайтиУсі(Predicate<Claim> співставлення) => this.FindAll(співставлення);
+
+	public virtual IEnumerable<Claim> ЗнайтиУсі(string тип) => this.FindAll(тип);
+
+	public virtual Claim ЗнайтиПерше(string тип) => this.FindFirst(тип);
+
+	public virtual Claim ЗнайтиПерше(Predicate<Claim> співставлення) => this.FindFirst(співставлення);
+
+	public virtual bool ІснуєСтверждення(Predicate<Claim> співставлення) => this.HasClaim(співставлення);
+
+	public virtual bool ІснуєСтверждення(string тип, string значення) => this.HasClaim(тип, значення);
+
+	public virtual bool УРолі(string роль) => this.IsInRole(роль);
+}

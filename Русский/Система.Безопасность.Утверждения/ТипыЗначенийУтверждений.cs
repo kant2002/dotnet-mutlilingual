8ed72f5f06e8daf9eb6f0a794f64ea6f4c72f449
Andrii Kurdiumov <kant2002@gmail.com>
2024-01-09 10:10:02 +0600

    Добавил перевод на русский

---
+++ Русский/Система.Безопасность.Утверждения/ТипыЗначенийУтверждений.cs
@@ -0,0 +1,145 @@
+﻿using System.Security.Claims;
+
+namespace Система.Безопасность.Утверждения;
+
+/// <summary>
+/// Определяет типы значений для утверждений в соответствии с URI типам, определенных W3C и OASIS.
+/// Этот класс нельзя наследовать.
+/// </summary>
+public static class ТипыЗначенийУтверждений
+{
+	/// <summary>
+	/// URI, который представляет тип данных XML base64Binary.
+	/// </summary>
+	public static string ДвоичныеБаза64 => ClaimValueTypes.Base64Binary;
+
+	/// <summary>
+	/// URI, который представляет тип данных SOAP UPN.
+	/// </summary>
+	public static string ИмяUpn => ClaimValueTypes.UpnName;
+
+	/// <summary>
+	/// URI, который представляет тип данных XML uinteger64.
+	/// </summary>
+	public static string БЦелое64 => ClaimValueTypes.UInteger64;
+
+	/// <summary>
+	/// URI, который представляет тип данных XML uinteger32.
+	/// </summary>
+	public static string БЦелое32 => ClaimValueTypes.UInteger32;
+
+	/// <summary>
+	/// URI, который представляет тип данных XML time.
+	/// </summary>
+	public static string Время => ClaimValueTypes.Time;
+
+	/// <summary>
+	/// URI, который представляет тип данных XML string.
+	/// </summary>
+	public static string Строка => ClaimValueTypes.String;
+
+	/// <summary>
+	/// URI, который представляет тип данных XML sid.
+	/// </summary>
+	public static string Sid => ClaimValueTypes.Sid;
+
+	/// <summary>
+	/// URI, который представляет тип данных RSAKeyValue XML Signature.
+	/// </summary>
+	public static string RsaKeyValue => ClaimValueTypes.RsaKeyValue;
+
+	/// <summary>
+	/// URI, который представляет тип данных rsa SOAP.
+	/// </summary>
+	public static string Rsa => ClaimValueTypes.Rsa;
+
+	/// <summary>
+	/// URI, который представляет тип данных rfc822Name XACML 1.0.
+	/// </summary>
+	public static string Rfc822Name => ClaimValueTypes.Rfc822Name;
+
+	/// <summary>
+	/// URI, который представляет тип данных KeyInfo XML Signature.
+	/// </summary>
+	public static string KeyInfo => ClaimValueTypes.KeyInfo;
+
+	/// <summary>
+	/// URI, который представляет тип данных XML integer64.
+	/// </summary>
+	public static string Целое64 => ClaimValueTypes.Integer64;
+
+	/// <summary>
+	/// URI, который представляет тип данных x500Name XACML 1.0.
+	/// </summary>
+	public static string X500Name => ClaimValueTypes.X500Name;
+
+	/// <summary>
+	/// URI, который представляет тип данных XML integer32.
+	/// </summary>
+	public static string Целое32 => ClaimValueTypes.Integer32;
+
+	/// <summary>
+	/// URI, который представляет тип данных XML hexBinary.
+	/// </summary>
+	public static string ДвоичныйХекс => ClaimValueTypes.HexBinary;
+
+	/// <summary>
+	/// URI, который представляет тип данных XML fqbn.
+	/// </summary>
+	public static string Fqbn => ClaimValueTypes.Fqbn;
+
+	/// <summary>
+	/// URI, который представляет тип данных SOAP emailaddress.
+	/// </summary>
+	public static string Почта => ClaimValueTypes.Email;
+
+	/// <summary>
+	/// URI, который представляет тип данных DSAKeyValue XML Signature.
+	/// </summary>
+	public static string DsaKeyValue => ClaimValueTypes.DsaKeyValue;
+
+	/// <summary>
+	/// URI, который представляет тип данных XML double.
+	/// </summary>
+	public static string Двойное => ClaimValueTypes.Double;
+
+	/// <summary>
+	/// URI, который представляет тип данных SOAP dns.
+	/// </summary>
+	public static string ИмяDns => ClaimValueTypes.DnsName;
+
+	/// <summary>
+	/// URI, который представляет тип данных XQuery daytimeDuration.
+	/// </summary>
+	public static string DaytimeDuration => ClaimValueTypes.DaytimeDuration;
+
+	/// <summary>
+	/// URI, который представляет тип данных XML dateTime.
+	/// </summary>
+	public static string ДатаИВремя => ClaimValueTypes.DateTime;
+
+	/// <summary>
+	/// URI, который представляет тип данных XML date.
+	/// </summary>
+	public static string Дата => ClaimValueTypes.Date;
+
+	/// <summary>
+	/// URI, который представляет тип данных XML boolean.
+	/// </summary>
+	public static string Логичное => ClaimValueTypes.Boolean;
+
+	/// <summary>
+	/// URI, который представляет тип данных XML base64Octet.
+	/// </summary>
+	public static string ОктетБаза64 => ClaimValueTypes.Base64Octet;
+
+	/// <summary>
+	/// URI, который представляет тип данных XML integer.
+	/// </summary>
+	public static string Целое => ClaimValueTypes.Integer;
+
+	/// <summary>
+	/// URI, который представляет тип данных XQuery yearMonthDuration.
+	/// </summary>
+	public static string YearMonthDuration => ClaimValueTypes.YearMonthDuration;
+}
---
+++ Русский/Система.Безопасность.Утверждения/ТипыУтверждений.cs
@@ -0,0 +1,111 @@
+﻿using System.Security.Claims;
+
+namespace Система.Безопасность.Утверждения;
+
+/// <summary>
+/// Определяет константы для известных типов утверждений, которые можна назначить субъекту.
+/// Этот класс нельзя наследовать.
+/// </summary>
+internal sealed class ТипыУтверждений
+{
+	/// <summary>
+	/// http://schemas.xmlsoap.org/ws/2005/05/identity/claims/actor.
+	/// </summary>
+	public static string Актор => ClaimTypes.Actor;
+
+	/// <summary>
+	/// URI для утверждения, которое обозначает почтовий индекс организации, http://schemas.xmlsoap.org/ws/2005/05/identity/claims/postalcode.
+	/// </summary>
+	public static string Индекс => ClaimTypes.PostalCode;
+
+	/// <summary>
+	/// URI для утверждения, которое обозначает главный SID группы сущности, http://schemas.xmlsoap.org/ws/2005/05/identity/claims/primarygroupsid.
+	/// </summary>
+	public static string ГлавныйSidГрупи => ClaimTypes.PrimaryGroupSid;
+
+	/// <summary>
+	/// URI для утверждения, которое обозначает главный SID сущности, http://schemas.xmlsoap.org/ws/2005/05/identity/claims/primarysid.
+	/// </summary>
+	public static string ГлавныйSid => ClaimTypes.PrimarySid;
+
+	/// <summary>
+	/// URI для утверждения, которое обозначает роль сущности, http://schemas.xmlsoap.org/ws/2005/05/identity/claims/role.
+	/// </summary>
+	public static string Роль => ClaimTypes.Role;
+
+	/// <summary>
+	/// URI для утверждения, которое обозначает RSA ключ, http://schemas.xmlsoap.org/ws/2005/05/identity/claims/rsa.
+	/// </summary>
+	public static string Rsa => ClaimTypes.Rsa;
+
+	/// <summary>
+	/// URI для утверждения, которое обозначает серийный номер, http://schemas.xmlsoap.org/ws/2005/05/identity/claims/serialnumber.
+	/// </summary>
+	public static string СерийныйНомер => ClaimTypes.SerialNumber;
+
+	/// <summary>
+	/// URI для утверждения, которое обозначает идентификатор безопасности (SID), http://schemas.xmlsoap.org/ws/2005/05/identity/claims/sid.
+	/// </summary>
+	public static string Sid => ClaimTypes.Sid;
+
+	/// <summary>
+	/// URI для утверждения, которое обозначает утверждение имени принципала служби (SPN), http://schemas.xmlsoap.org/ws/2005/05/identity/claims/spn.
+	/// </summary>
+	public static string Spn => ClaimTypes.Spn;
+
+	/// <summary>
+	/// URI для утверждения, которое обозначает штат или провинцию, в которой проживает субъект, http://schemas.xmlsoap.org/ws/2005/05/identity/claims/stateorprovince.
+	/// </summary>
+	public static string ШтатИлиПровинция => ClaimTypes.StateOrProvince;
+
+	/// <summary>
+	/// URI для утверждения, которое обозначает адрес сущности, http://schemas.xmlsoap.org/ws/2005/05/identity/claims/streetaddress.
+	/// </summary>
+	public static string Адрес => ClaimTypes.StreetAddress;
+
+	/// <summary>
+	/// URI для утверждения, которое обозначает фамилию сущности, http://schemas.xmlsoap.org/ws/2005/05/identity/claims/surname.
+	/// </summary>
+	public static string Фамилия => ClaimTypes.Surname;
+
+	/// <summary>
+	/// URI для утверждения, которое обозначает системну сущность, http://schemas.xmlsoap.org/ws/2005/05/identity/claims/system.
+	/// </summary>
+	public static string Система => ClaimTypes.System;
+
+	/// <summary>
+	/// URI для утверждения, которое обозначает цифровой отпечаток, http://schemas.xmlsoap.org/ws/2005/05/identity/claims/thumbprint.
+	/// Отпечаток – это глобально уникальний хеш SHA-1 сертификата X.509.
+	/// </summary>
+	public static string Отпечаток => ClaimTypes.Thumbprint;
+
+	/// <summary>
+	/// URI для утверждения, которое обозначает основное имя пользователя (UPN), http://schemas.xmlsoap.org/ws/2005/05/identity/claims/role.
+	/// </summary>
+	public static string Upn => ClaimTypes.Upn;
+
+	/// <summary>
+	/// URI для утверждения, которое обозначает роль URI, http://schemas.xmlsoap.org/ws/2005/05/identity/claims/uri.
+	/// </summary>
+	public static string Uri => ClaimTypes.Uri;
+
+	/// <summary>
+	/// http://schemas.xmlsoap.org/ws/2005/05/identity/claims/userdata.
+	/// </summary>
+	public static string ДанныеПользователя => ClaimTypes.UserData;
+
+	/// <summary>
+	/// http://schemas.xmlsoap.org/ws/2005/05/identity/claims/version.
+	/// </summary>
+	public static string Версия => ClaimTypes.Version;
+
+	/// <summary>
+	/// URI для утверждения, которое обозначает веб-страницу сущности, http://schemas.xmlsoap.org/ws/2005/05/identity/claims/webpage.
+	/// </summary>
+	public static string ВебСтраница => ClaimTypes.Webpage;
+
+	/// <summary>
+	/// URI для утверждения, которое обозначает название сущности, http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name.
+	/// </summary>
+	public static string Имя => ClaimTypes.Name;
+}

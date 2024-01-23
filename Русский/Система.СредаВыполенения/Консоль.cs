e3823210ab88d78ef197e68d38e94db452aebdbe
Andrii Kurdiumov <kant2002@gmail.com>
2024-01-23 19:05:37 +0600

    Добавил окружение и консоль

---
+++ Русский/Система.СредаВыполенения/Консоль.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using static System.Console;
+
+namespace Система;
+
+public class Консоль
+{
+    public static bool ВводПеренаправлен => IsInputRedirected;
+    public static int ВысотаБуфера { get => BufferHeight; set => BufferHeight = value; }
+    public static int ШиринаБуфера { get => BufferWidth; set => BufferWidth = value; }
+    public static bool БлокЗагл => CapsLock;
+    public static int ЛевоКурсора { get => CursorLeft; set => CursorLeft = value; }
+    public static int РазмерКурсора { get => CursorSize; set => CursorSize = value; }
+    public static int ВерхКурсора { get => CursorTop; set => CursorTop = value; }
+    public static bool КурсорВидим { get => CursorVisible; set => CursorVisible = value; }
+    public static TextWriter Ошибка => Error;
+    public static ConsoleColor ЦветПереднегоПлана { get => ForegroundColor; set => ForegroundColor = value; }
+    public static TextReader Ввод => In;
+    public static Encoding КодировкаВвода { get => InputEncoding; set => InputEncoding = value; }
+    public static bool ОшибкаПеренаправлена => IsErrorRedirected;
+    public static int ШиринаОкна { get => WindowWidth; set => WindowWidth = value; }
+    public static bool ВыводПеренаправлен => IsOutputRedirected;
+    public static bool КнопкаДоступна => KeyAvailable;
+    public static int НаибольшаяВысотаОкна => LargestWindowHeight;
+    public static int НаибольшаяШиринаОкна => LargestWindowWidth;
+    public static bool БлокЦифр => NumberLock;
+    public static TextWriter Вывод => Out;
+    public static Encoding КодировкаВывода { get => OutputEncoding; set => OutputEncoding = value; }
+    public static string Заголовок { get => Title; set => Title = value; }
+    public static bool ОбрабатыватьControlCКакВвод { get => TreatControlCAsInput; set => TreatControlCAsInput = value; }
+    public static int ВысотаОкна { get => WindowHeight; set => WindowHeight = value; }
+    public static int ЛевоОкна { get => WindowLeft; set => WindowLeft = value; }
+    public static int ВерхОкна { get => WindowTop; set => WindowTop = value; }
+    public static ConsoleColor ФоновыйЦвет { get => BackgroundColor; set => BackgroundColor = value; }
+
+    public static event ConsoleCancelEventHandler КнопкаОтменыНажата
+    {
+        add { CancelKeyPress += value; }
+        remove { CancelKeyPress -= value; }
+    }
+
+    public static void Гудок() => Beep();
+    public static void Гудок(int частота, int длительность) => Beep(частота, длительность);
+    public static void Очистить() => Clear();
+    public static void ПереместитьОбластьБуфера(int источникЛево, int источникВерх, int источникШирина, int источникВысота, int получательЛево, int получательВерх) => MoveBufferArea(источникЛево, источникВерх, источникШирина, источникВысота, получательЛево, получательВерх);
+    public static void ПереместитьОбластьБуфера(int источникЛево, int источникВерх, int источникШирина, int источникВысота, int получательЛево, int получательВерх, char источникСимвол, ConsoleColor источникПередЦвет, ConsoleColor источникФонЦвет) => MoveBufferArea(источникЛево, источникВерх, источникШирина, источникВысота, получательЛево, получательВерх, источникСимвол, источникПередЦвет, источникФонЦвет);
+
+    public static Stream ОткрытьСтандартнуюОшибку(int размерБуфера) => OpenStandardError(размерБуфера);
+    public static Stream ОткрытьСтандартнуюОшибку() => OpenStandardError();
+    public static Stream ОткрытьСтандартныйВвод(int размерБуфера) => OpenStandardInput(размерБуфера);
+    public static Stream ОткрытьСтандартныйВвод() => OpenStandardInput();
+    public static Stream ОткрытьСтандартныйВывод(int размерБуфера) => OpenStandardOutput(размерБуфера);
+    public static Stream ОткрытьСтандартныйВывод() => OpenStandardOutput();
+    public static int Прочитать() => Read();
+    public static ConsoleKeyInfo ПрочитатьКнопку(bool перехватить) => ReadKey(перехватить);
+    public static ConsoleKeyInfo ПрочитатьКнопку() => ReadKey();
+    public static string ПрочитатьСтроку() => ReadLine();
+    public static void СброситьЦвет() => ResetColor();
+    public static void УстановитьРазмерБуфера(int ширина, int высота) => SetBufferSize(ширина, высота);
+    public static void УстановитьПозициюКурсора(int лево, int верх) => SetCursorPosition(лево, верх);
+    public static void УстановитьОшибку(TextWriter newError) => SetError(newError);
+    public static void УстановитьВвод(TextReader newIn) => SetIn(newIn);
+    public static void УстановитьВывод(TextWriter newOut) => SetOut(newOut);
+    public static void УстановитьПозициюОкна(int лево, int верх) => SetWindowPosition(лево, верх);
+    public static void УстановитьРазмерОкна(int ширина, int высота) => SetWindowSize(ширина, высота);
+    public static void Написать(ulong значение) => Write(значение);
+    public static void Написать(bool значение) => Write(значение);
+    public static void Написать(char значение) => Write(значение);
+    public static void Написать(char[] буфер) => Write(буфер);
+    public static void Написать(char[] буфер, int индекс, int количество) => Write(буфер, индекс, количество);
+    public static void Написать(double значение) => Write(значение);
+    public static void Написать(long значение) => Write(значение);
+    public static void Написать(object значение) => Write(значение);
+    public static void Написать(float значение) => Write(значение);
+    public static void Написать(string значение) => Write(значение);
+    public static void Написать(string формат, object арг0) => Write(формат, арг0);
+    public static void Написать(string формат, object арг0, object арг1) => Write(формат, арг0, арг1);
+    public static void Написать(string формат, object арг0, object арг1, object арг2) => Write(формат, арг0, арг1, арг2);
+    public static void Написать(string формат, params object[] арг) => Write(формат, арг);
+    public static void Написать(uint значение) => Write(значение);
+    public static void Написать(decimal значение) => Write(значение);
+    public static void Написать(int значение) => Write(значение);
+    public static void НаписатьЛинию(ulong значение) => WriteLine(значение);
+    public static void НаписатьЛинию(string формат, params object[] арг) => WriteLine(формат, арг);
+    public static void НаписатьЛинию() => WriteLine();
+    public static void НаписатьЛинию(bool значение) => WriteLine(значение);
+    public static void НаписатьЛинию(char[] буфер) => WriteLine(буфер);
+    public static void НаписатьЛинию(char[] буфер, int индекс, int количество) => WriteLine(буфер, индекс, количество);
+    public static void НаписатьЛинию(decimal значение) => WriteLine(значение);
+    public static void НаписатьЛинию(double значение) => WriteLine(значение);
+    public static void НаписатьЛинию(uint значение) => WriteLine(значение);
+    public static void НаписатьЛинию(int значение) => WriteLine(значение);
+    public static void НаписатьЛинию(object значение) => WriteLine(значение);
+    public static void НаписатьЛинию(float значение) => WriteLine(значение);
+    public static void НаписатьЛинию(string значение) => WriteLine(значение);
+    public static void НаписатьЛинию(string формат, object арг0) => WriteLine(формат, арг0);
+    public static void НаписатьЛинию(string формат, object арг0, object арг1) => WriteLine(формат, арг0, арг1);
+    public static void НаписатьЛинию(string формат, object арг0, object арг1, object арг2) => WriteLine(формат, арг0, арг1, арг2);
+    public static void НаписатьЛинию(long значение) => WriteLine(значение);
+    public static void НаписатьЛинию(char значение) => WriteLine(значение);
+}
---
+++ Русский/Система.СредаВыполенения/Окружение.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections;
+using static System.Environment;
+
+namespace Система;
+
+public static class Окружение
+{
+    public static int КодВыхода { get => ExitCode; set => ExitCode = value; }
+    public static bool Есть64БитнимПроцессом => Is64BitProcess;
+    public static int ТекущийИдУправляемогоПотока => CurrentManagedThreadId;
+    public static string ТекущаяДиректория { get => CurrentDirectory; set => CurrentDirectory = value; }
+    public static string КоманднаяСтрока => CommandLine;
+    public static string ИмяМашины => MachineName;
+    public static string НоваяСтрока => NewLine;
+    public static OperatingSystem ВерсияОС => OSVersion;
+    public static int КоличествоПроцессоров => ProcessorCount;
+    public static string ТрассировкаСтека => StackTrace;
+    public static string СистемнаяДиректория => SystemDirectory;
+    public static int РазмерСистемнойСтраницы => SystemPageSize;
+    public static int КоличествоТиков => TickCount;
+    public static string ИмяПользователяДомена => UserDomainName;
+    public static bool ИнтерактивныйПользователь => UserInteractive;
+    public static string ИмяПользователя => UserName;
+    public static Version Версия => Environment.Version;
+    public static long РабочийНабор => WorkingSet;
+    public static bool ВыключениеНачалось => HasShutdownStarted;
+    public static bool Есть64БитнойОперационноюСистемой => Is64BitOperatingSystem;
+    public static void Выход(int кодВыходу) => Exit(кодВыходу);
+    public static string РазвернутьПеременныеОкружения(string имя) => ExpandEnvironmentVariables(имя);
+    public static void УпастьБыстро(string сообщение, Exception исключение) => FailFast(сообщение, исключение);
+    public static void УпастьБыстро(string сообщение) => FailFast(сообщение);
+    public static string[] ПолучитьАргументиКомандногоРядка() => GetCommandLineArgs();
+    public static string ПолучитьПеременнуюОкружения(string переменная, EnvironmentVariableTarget цель) => GetEnvironmentVariable(переменная, цель);
+    public static string ПолучитьПеременнуюОкружения(string переменная) => GetEnvironmentVariable(переменная);
+    public static IDictionary ПолучитьПеременныеОкружения(EnvironmentVariableTarget цель) => GetEnvironmentVariables(цель);
+    public static IDictionary ПолучитьПеременныеОкружения() => GetEnvironmentVariables();
+    public static string ПолучитьПутьПапки(СпециальнаяПапка папка, ПараметрыСпециальнойПапки option) => GetFolderPath((SpecialFolder)(int)папка, (SpecialFolderOption)(int)option);
+    public static string ПолучитьПутьПапки(СпециальнаяПапка папка) => GetFolderPath((SpecialFolder)(int)папка);
+    public static string[] ПолучитьЛогическиеДиски() => GetLogicalDrives();
+    public static void ВстановитиПеременнуюОкружения(string переменная, string значение, EnvironmentVariableTarget цель) => SetEnvironmentVariable(переменная, значение, цель);
+    public static void ВстановитиПеременнуюОкружения(string переменная, string значение) => SetEnvironmentVariable(переменная, значение);
+
+    public enum СпециальнаяПапка
+    {
+        РобочийСтол = 0,
+        Программы = 2,
+        МоиДокументы = 5,
+        Personal = 5,
+        Favorites = 6,
+        Startup = 7,
+        Recent = 8,
+        SendTo = 9,
+        StartMenu = 11,
+        MyMusic = 13,
+        MyVideos = 14,
+        DesktopDirectory = 16,
+        MyComputer = 17,
+        NetworkShortcuts = 19,
+        Fonts = 20,
+        Templates = 21,
+        CommonStartMenu = 22,
+        CommonPrograms = 23,
+        CommonStartup = 24,
+        CommonDesktopDirectory = 25,
+        ApplicationData = 26,
+        PrinterShortcuts = 27,
+        LocalApplicationData = 28,
+        InternetCache = 32,
+        Cookies = 33,
+        History = 34,
+        CommonApplicationData = 35,
+        System = 37,
+        ProgramFiles = 38,
+        MyPictures = 39,
+        UserProfile = 40,
+        SystemX86 = 41,
+        ProgramFilesX86 = 42,
+        CommonProgramFiles = 43,
+        CommonProgramFilesX86 = 44,
+        CommonTemplates = 45,
+        CommonDocuments = 46,
+        CommonAdminTools = 47,
+        AdminTools = 48,
+        CommonMusic = 53,
+        CommonPictures = 54,
+        CommonVideos = 55,
+        Resources = 56,
+        LocalizedResources = 57,
+        CommonOemLinks = 58,
+        CDBurning = 59
+    }
+    public enum ПараметрыСпециальнойПапки
+    {
+        Никаких = 0,
+        НеПроверять = 16384,
+        Создать = 32768
+    }
+}

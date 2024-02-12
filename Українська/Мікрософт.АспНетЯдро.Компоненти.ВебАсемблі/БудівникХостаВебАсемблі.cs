2d7d9d97b52cd45814d74b5adf6d0f3948a09b42
Andrii Kurdiumov <kant2002@gmail.com>
2024-02-12 14:48:48 +0600

    Переклад `Microsoft.AspNetCore.Components.WebAssembly` на українську.

---
+++ Українська/Мікрософт.АспНетЯдро.Компоненти.ВебАсемблі/БудівникХостаВебАсемблі.cs
@@ -0,0 +1,31 @@
+﻿namespace Мікрософт.АспНетЯдро.Компоненти.ВебАсемблі;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+public sealed class БудівникХостаВебАсемблі
+{
+    private WebAssemblyHostBuilder webAssemblyHostBuilder;
+
+    internal БудівникХостаВебАсемблі(WebAssemblyHostBuilder webAssemblyHostBuilder)
+    {
+        this.webAssemblyHostBuilder = webAssemblyHostBuilder;
+    }
+
+    public WebAssemblyHostConfiguration Конфігурація => webAssemblyHostBuilder.Configuration;
+
+    public RootComponentMappingCollection КорневіКомпоненти => webAssemblyHostBuilder.RootComponents;
+
+    public IServiceCollection Сервіси => webAssemblyHostBuilder.Services;
+
+    public IWebAssemblyHostEnvironment ОточенняХоста => webAssemblyHostBuilder.HostEnvironment;
+
+    public ILoggingBuilder Протоколювання => webAssemblyHostBuilder.Logging;
+
+    public static БудівникХостаВебАсемблі CreateDefault(string[]? args = null) => new (WebAssemblyHostBuilder.CreateDefault(args));
+
+    public void ConfigureContainer<TBuilder>(IServiceProviderFactory<TBuilder> factory, Action<TBuilder>? configure = null)
+        where TBuilder: notnull
+        => webAssemblyHostBuilder.ConfigureContainer(factory, configure);
+    public ХостВебАсемблі Build() => new (webAssemblyHostBuilder.Build());
+}
---
+++ Українська/Мікрософт.АспНетЯдро.Компоненти.ВебАсемблі/ХостВебАсемблі.cs
@@ -0,0 +1,21 @@
+﻿namespace Мікрософт.АспНетЯдро.Компоненти.ВебАсемблі;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
+
+public sealed class ХостВебАсемблі : IAsyncDisposable
+{
+    private readonly WebAssemblyHost webAssemblyHost;
+
+    internal ХостВебАсемблі(WebAssemblyHost webAssemblyHost)
+    {
+        this.webAssemblyHost = webAssemblyHost;
+    }
+
+    public IConfiguration Конфігурація => this.webAssemblyHost.Configuration;
+
+    public IServiceProvider Сервіси => this.webAssemblyHost.Services;
+
+    public ValueTask DisposeAsync() => this.webAssemblyHost.DisposeAsync();
+
+    public Task ЗапуститиАсінх() => this.webAssemblyHost.RunAsync();
+}

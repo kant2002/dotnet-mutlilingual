3a21a99edadeb5b77f281e1d559d4ff5c4aa0d53
Andrii Kurdiumov <kant2002@gmail.com>
2024-02-13 12:25:03 +0600

    Переклад `Microsoft.AspNetCore.Components.Web` на українську

---
+++ Українська/Мікрософт.АспНетЯдро.Компоненти.Веб/РежимРендера.cs
@@ -0,0 +1,10 @@
+﻿namespace Мікрософт.АспНетЯдро.Компоненти.Веб;
+using Microsoft.AspNetCore.Components.Web;
+
+
+public static class РежимРендера
+{
+    public static InteractiveServerRenderMode ІнтерактивнийСервер => RenderMode.InteractiveServer;
+    public static InteractiveWebAssemblyRenderMode ІнтерактивнийВебАсемблі => RenderMode.InteractiveWebAssembly;
+    public static InteractiveAutoRenderMode ІнтерактивнийАвто => RenderMode.InteractiveAuto;
+}

cd21f5a310a389dc27fb60460fb10f44c21825f7
Andrii Kurdiumov <kant2002@gmail.com>
2024-04-01 20:22:13 +0500

    Додав переклад `Microsoft.AspNetCore.HttpLogging` на українську

---
+++ Українська/Мікрософт.АспНетЯдро.HttpПротоколювання/БудівникHttpПротоколювання.cs
@@ -0,0 +1,9 @@
+﻿namespace Мікрософт.АспНетЯдро.Будівник;
+using Microsoft.AspNetCore.Builder;
+
+public static class РозширенняБудівникаHttpПротоколювання
+{
+	public static IApplicationBuilder ВикористовуватиHttpПротоколювання(this IApplicationBuilder апка) => HttpLoggingBuilderExtensions.UseHttpLogging(апка);
+	public static IApplicationBuilder ВикористовуватиW3CПротоколювання(this IApplicationBuilder апка) => HttpLoggingBuilderExtensions.UseHttpLogging(апка);
+
+}
---
+++ Українська/Мікрософт.АспНетЯдро.HttpПротоколювання/РозширенняСервісівHttpПротоколювання.cs
@@ -0,0 +1,12 @@
+﻿namespace Мікрософт.Розширення.ВприскЗалежностей;
+
+using Microsoft.AspNetCore.HttpLogging;
+using Microsoft.Extensions.DependencyInjection;
+using System.Diagnostics.CodeAnalysis;
+
+public static class РозширенняСервісівHttpПротоколювання
+{
+	public static IServiceCollection ДодатиHttpПротоколювання(this IServiceCollection сервіси, Action<HttpLoggingOptions> налаштувантиОпції) => сервіси.AddHttpLogging(налаштувантиОпції);
+	public static IServiceCollection ДодатиПерехоплювачHttpПротоколювання<[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicConstructors)] T>(this IServiceCollection сервіси) where T : class, IHttpLoggingInterceptor => сервіси.AddHttpLoggingInterceptor<T>();
+	public static IServiceCollection ДодатиW3CПротоколювання(this IServiceCollection сервіси, Action<W3CLoggerOptions> налаштувантиОпції) => сервіси.AddW3CLogging(налаштувантиОпції);
+}

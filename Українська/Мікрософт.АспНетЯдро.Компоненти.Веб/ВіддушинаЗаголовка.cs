a80b2bf80d6bc54fefff789f604b8344d052611c
Andrii Kurdiumov <kant2002@gmail.com>
2024-05-09 15:35:52 +0500

    Додав компонент для відображення назви сторінки

---
+++ Українська/Мікрософт.АспНетЯдро.Компоненти.Веб/ВіддушинаЗаголовка.cs
@@ -0,0 +1,55 @@
+﻿namespace Мікрософт.АспНетЯдро.Компоненти.Веб;
+using Microsoft.AspNetCore.Components.Rendering;
+using Microsoft.AspNetCore.Components.Sections;
+
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+public sealed class ВіддушинаЗаголовка : ComponentBase
+{
+    private const string GetAndRemoveExistingTitle = "Blazor._internal.PageTitle.getAndRemoveExistingTitle";
+
+    internal static readonly object ИдСекціїЗаголовка = new object();
+
+    internal static readonly object ИдСекціїНазви = new object();
+
+    private string _defaultTitle;
+
+    [Inject]
+    private IJSRuntime JSRuntime { get; set; }
+
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        if (firstRender)
+        {
+            _defaultTitle = await JSRuntimeExtensions.InvokeAsync<string>(JSRuntime, "Blazor._internal.PageTitle.getAndRemoveExistingTitle", Array.Empty<object>());
+            StateHasChanged();
+        }
+    }
+
+    protected override void BuildRenderTree(RenderTreeBuilder builder)
+    {
+        builder.OpenComponent<SectionOutlet>(0);
+        builder.AddComponentParameter(1, "SectionId", ИдСекціїНазви);
+        builder.CloseComponent();
+        if (!string.IsNullOrEmpty(_defaultTitle))
+        {
+            builder.OpenComponent<SectionContent>(2);
+            builder.AddComponentParameter(3, "SectionId", ИдСекціїНазви);
+            builder.AddComponentParameter(4, "IsDefaultContent", true);
+            builder.AddComponentParameter(5, "ChildContent", new RenderFragment(BuildDefaultTitleRenderTree));
+            builder.CloseComponent();
+        }
+
+        builder.OpenComponent<SectionOutlet>(6);
+        builder.AddComponentParameter(7, "SectionId", ИдСекціїЗаголовка);
+        builder.CloseComponent();
+    }
+
+    private void BuildDefaultTitleRenderTree(RenderTreeBuilder builder)
+    {
+        builder.OpenElement(0, "title");
+        builder.AddContent(1, _defaultTitle);
+        builder.CloseElement();
+    }
+}
---
+++ Українська/Мікрософт.АспНетЯдро.Компоненти.Веб/НазваСторінки.cs
@@ -0,0 +1,33 @@
+﻿namespace Мікрософт.АспНетЯдро.Компоненти.Веб;
+
+using Microsoft.AspNetCore.Components.Rendering;
+using Microsoft.AspNetCore.Components.Sections;
+using Microsoft.AspNetCore.Components;
+
+/// <summary>
+/// Enables rendering an HTML <c>&lt;title&gt;</c> to a <see cref="ВіддушинаЗаголовка"/> component.
+/// </summary>
+public sealed class НазваСторінки : ComponentBase
+{
+    /// <summary>
+    /// Gets or sets the content to be rendered as the document title.
+    /// </summary>
+    [Parameter]
+    public RenderFragment? ChildContent { get; set; }
+
+    /// <inheritdoc/>
+    protected override void BuildRenderTree(RenderTreeBuilder builder)
+    {
+        builder.OpenComponent<SectionContent>(0);
+        builder.AddComponentParameter(1, nameof(SectionContent.SectionId), ВіддушинаЗаголовка.ИдСекціїНазви);
+        builder.AddComponentParameter(2, nameof(SectionContent.ChildContent), (RenderFragment)BuildTitleRenderTree);
+        builder.CloseComponent();
+    }
+
+    private void BuildTitleRenderTree(RenderTreeBuilder builder)
+    {
+        builder.OpenElement(0, "title");
+        builder.AddContent(1, ChildContent);
+        builder.CloseElement();
+    }
+}
